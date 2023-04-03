42d4bfaa025c82e4b8c93b15f3893f33023048e7
MrinalAppentus <mrinal@appentus.me>
2023-04-03 12:40:46 +0530

    Initial commit

---
+++ app/src/main/java/com/pos_system/sale_record/utils/WCharMapperCT42BNazanin.java
@@ -0,0 +1,6 @@
+package com.pos_system.sale_record.utils;
+
+public class WCharMapperCT42BNazanin extends WCharMapperCT41{
+
+
+}
---
+++ app/src/main/java/com/pos_system/sale_record/utils/WCharMapperCT43CustomFont.java
@@ -0,0 +1,5 @@
+package com.pos_system.sale_record.utils;
+
+public class WCharMapperCT43CustomFont extends WCharMapperCT41{
+
+}
